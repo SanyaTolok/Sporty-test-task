f0024f97150965f0c5646b61bf5f1cce1300e1b6
Oleksandr Tolok <alex@centriqhome.com>
2025-06-20 18:06:26 +0200

    Updated test Updated config

--- src/tests/test.py
+++ src/tests/test.py
@@ -5,40 +5,30 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.chrome.options import Options as ChromeOptions
-
-# The webdriver management will be handled by the browserstack-sdk
-# so this will be overridden and tests will run browserstack -
-# without any changes to the test files!
 options = ChromeOptions()
-options.set_capability('sessionName', 'BStack Sample Test')
+options.set_capability('sessionName', 'UI test example')
 driver = webdriver.Chrome(options=options)
 
 try:
-    driver.get('https://bstackdemo.com/')
-    WebDriverWait(driver, 10).until(EC.title_contains('StackDemo'))
-    # Get text of an product - iPhone 12
-    item_on_page = WebDriverWait(driver, 10).until(
-        EC.visibility_of_element_located((By.XPATH, '//*[@id="1"]/p'))).text
-    # Click the 'Add to cart' button if it is visible
+    # Navigate to twitch page
+    driver.get('https://www.twitch.tv/')
+    # wait for the page to load and title to contain 'Twitch'
+    WebDriverWait(driver, 10).until(EC.title_contains('Twitch'))
+    # Click search icon
     WebDriverWait(driver, 10).until(EC.visibility_of_element_located(
-        (By.XPATH, '//*[@id="1"]/div[4]'))).click()
-    # Check if the Cart pane is visible
+        (By.XPATH, '//*[@id="root"]/div[2]/a[2]/div/div[1]/svg'))).click()
+     # wait for the page to load and search bar to exist and enter search term
     WebDriverWait(driver, 10).until(EC.visibility_of_element_located(
-        (By.CLASS_NAME, 'float-cart__content')))
-    # Get text of product in cart
-    item_in_cart = WebDriverWait(driver, 10).until(EC.visibility_of_element_located(
-        (By.XPATH, '//*[@id="__next"]/div/div/div[2]/div[2]/div[2]/div/div[3]/p[1]'))).text
-    # Verify whether the product (iPhone 12) is added to cart
-    if item_on_page == item_in_cart:
-        # Set the status of test as 'passed' if item is added to cart
-        driver.execute_script(
-            'browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"passed", "reason": "iPhone 12 has been successfully added to the cart!"}}')
-    else:
-        # Set the status of test as 'failed' if item is not added to cart
-        driver.execute_script(
-            'browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed", "reason": "iPhone 12 not added to the cart!"}}')
+        (By.XPATH, '//input[@type="search"]'))).send_keys('StarCraft II')
+    # scroll down two times to load more results
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    # wait for the search results to load
+    WebDriverWait(driver, 10).until(EC.visibility_of_element_located(
+        (By.XPATH, '//div[@data-a-target="search-result-game"]')))
 except NoSuchElementException as err:
     message = 'Exception: ' + str(err.__class__) + str(err.msg)
+    # If an element is not found, set the session status to failed
     driver.execute_script(
         'browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed", "reason": ' + json.dumps(message) + '}}')
 except Exception as err:
